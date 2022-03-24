a3c90719beb3c72c6c438c93ab225ca5878a63ce
lomik31 <radiostepan@gmail.com>
2022-03-24 11:23:27 +0300

    adding files

---
+++ Лаба 2/task.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from random import randint
+def generateList(len):
+    ls = []
+    for _ in range(0, len):
+        ls.append(randint(-15, 15))
+    return ls
+def countValues(list):
+    counter = dict(Counter(list))
+    return counter
+def noName(dict):
+    list = []
+    for i in dict.keys():
+        if dict[i] > 1:
+            list.append(i)
+    return list
+def main():
+    list = noName(countValues(generateList(15)))
+    if list == []: return print("Нет повторяющихся значений")
+    print(max(list))
+if __name__ == "__main__":
+    main()
