2cad5d0c32c7ecb13eb84165e81c79bedc991b1d
lomik31 <radiostepan@gmail.com>
2022-04-17 00:40:29 +0300

    edit lab 1

--- Лаба 1/task.py
+++ Лаба 1/task.py
@@ -1,3 +1,8 @@
 from math import sqrt;
 def distance(x1, y1, x2, y2):
     return sqrt((x1 - x2)**2 + (y1 - y2)**2);
+
+def main():
+    print(distance(0.0, 0.0, 1.0, 1.0)) 
+if __name__ == "__main__":
+    main()
